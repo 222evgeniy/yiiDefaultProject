9ae3162eb53f2d38d5a459b65a9014276f87ae91
evgeniy <222evgeniy@gmail.com>
2022-03-16 13:17:15 +0200

    fix config - 1

--- common/config/main.php
+++ common/config/main.php
@@ -11,12 +11,6 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=mysql;dbname=yii2advanced',
-            'username' => 'yii2advanced',
-            'password' => 'secret',
-            'charset' => 'utf8',
-        ],
+
     ],
 ];
